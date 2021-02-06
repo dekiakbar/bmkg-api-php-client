f9a055dded3a5d439ae1a479c26f186aa0ec0e68
Deki Akbar <dekiakbar@linuxmail.org>
2021-02-06 18:43:59 +0700

    Apply fixes from StyleCI (#1)

--- src/Earthquake.php
+++ src/Earthquake.php
@@ -4,57 +4,61 @@ namespace Dekiakbar\BmkgApiPhpClient;
 
 class Earthquake
 {
-    const BASE_URL = "https://data.bmkg.go.id/";
-    const PREFIX_URL = array(
-        "autogempa" => "autogempa.xml",
-        "gempaterkini" => "gempaterkini.xml",
-        "gempadirasakan" => "gempadirasakan.xml",
-        "lasttsunami" => "lasttsunami.xml",
-        "en_autogempa" => "en_autogempa.xml",
-        "en_gempaterkini" => "en_gempaterkini.xml"
-    );
+    const BASE_URL = 'https://data.bmkg.go.id/';
+    const PREFIX_URL = [
+        'autogempa'       => 'autogempa.xml',
+        'gempaterkini'    => 'gempaterkini.xml',
+        'gempadirasakan'  => 'gempadirasakan.xml',
+        'lasttsunami'     => 'lasttsunami.xml',
+        'en_autogempa'    => 'en_autogempa.xml',
+        'en_gempaterkini' => 'en_gempaterkini.xml',
+    ];
 
     public $data;
 
-    public function execute($code=null){
-        if( $code != null){
-            if( array_key_exists($code, SELF::PREFIX_URL) ){
-                try{
-                    $xmlContents= file_get_contents(SELF::BASE_URL.SELF::PREFIX_URL[$code]);
+    public function execute($code = null)
+    {
+        if ($code != null) {
+            if (array_key_exists($code, self::PREFIX_URL)) {
+                try {
+                    $xmlContents = file_get_contents(self::BASE_URL.self::PREFIX_URL[$code]);
                     $this->data = simplexml_load_string($xmlContents, null, true);
 
                     return $this->objectBuilder();
-                    
-                }catch(\Exception $e){
+                } catch (\Exception $e) {
                     echo 'Caught exception: '.$e->getMessage()."\n";
                 }
-            }else{
-                throw new \Exception("Area code is wrong");
+            } else {
+                throw new \Exception('Area code is wrong');
             }
-        }else{
-            try{
-                $xmlContents= file_get_contents(SELF::BASE_URL.SELF::PREFIX_URL['autogempa']);
+        } else {
+            try {
+                $xmlContents = file_get_contents(self::BASE_URL.self::PREFIX_URL['autogempa']);
                 $this->data = simplexml_load_string($xmlContents, null, true);
 
                 return $this->objectBuilder();
-                
-            }catch(\Exception $e){
+            } catch (\Exception $e) {
                 echo 'Caught exception: '.$e->getMessage()."\n";
             }
         }
     }
 
-    public function objectBuilder(){
-        $this->data = json_decode( json_encode($this->data) );
+    public function objectBuilder()
+    {
+        $this->data = json_decode(json_encode($this->data));
+
         return $this;
     }
 
-    public function getData(){
+    public function getData()
+    {
         return $this->data;
     }
 
-    public function getCodeList(){
-        $this->data = array_keys(SELF::PREFIX_URL);
+    public function getCodeList()
+    {
+        $this->data = array_keys(self::PREFIX_URL);
+
         return $this->data;
     }
 }
